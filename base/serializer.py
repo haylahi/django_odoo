c050ed9248f45494a9448f70bfbaed4d138de20c
yaoyk <yyk1052433260@163.com>
2019-04-17 13:48:04 +0800

    [IMP] fix country

--- base/serializer.py
+++ base/serializer.py
@@ -16,16 +16,15 @@ def get_base_model(model_name: str):
 # -----------------------------------------------------------------------------
 
 
-class CountrySerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class CountrySerializer(serializers.ModelSerializer):
     name = serializers.CharField(label=get_field_str(BaseCountry, 'name'), validators=[UniqueValidator(queryset=BaseCountry.objects.filter(is_active=True))])
     short_name = serializers.CharField(label=get_field_str(BaseCountry, 'short_name'), validators=[UniqueValidator(queryset=BaseCountry.objects.filter(is_active=True))])
     area_code = serializers.CharField(label=get_field_str(BaseCountry, 'area_code'), validators=[UniqueValidator(queryset=BaseCountry.objects.filter(is_active=True))])
-    national_flag = serializers.ImageField(use_url=True)
     is_active = serializers.BooleanField(default=True)
 
-    def create(self, attrs: dict):
-        return BaseCountry.objects.create(**attrs)
+    class Meta:
+        model = BaseCountry
+        fields = '__all__'
 
     def update(self, instance, validated_data):
         raise serializers.ValidationError('Unsupported Operation: 你暂时不能更新国家的信息...')
--- powerful/settings.py
+++ powerful/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6
+    'PAGE_SIZE': 10
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
