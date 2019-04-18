55c6d8cb1a5a95ee457bc9fa119bd988ce28a043
yaoyk <yyk1052433260@163.com>
2019-04-18 09:11:59 +0800

    [FIX] some fix

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
+import random
+
 from django.http import HttpResponse
 from rest_framework import generics
 
 from base.serializer import CountrySerializer, ProvinceSerializer, CitySerializer
 from .models import BaseCountry, BaseProvince, BaseCity
+
+import logging
+
+_log = logging.getLogger(__name__)
 
 
 class CountryListCreateView(generics.ListCreateAPIView):
@@ -30,9 +36,12 @@ class CityListCreateView(generics.ListCreateAPIView):
 
 def test_test(request):
     from .tasks import task_test
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    ret = 'null'
     try:
-        r = task_test.delay(1, 2)
-        print(r.get(timeout=1))
-    except Exception:
-        pass
-    return HttpResponse('<h2>200 OK</h2> ', content_type='text/html; charset=utf-8', status=200)
+        task = task_test.delay(a, b)
+        ret = task.get(timeout=1)
+    except:
+        _log.error('任务执行失败')
+    return HttpResponse('<h2>200 OK</h2> {ret}'.format(ret=ret), content_type='text/html; charset=utf-8', status=200)
