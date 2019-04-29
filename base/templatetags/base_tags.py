bfeaacd50b7b4c3ffe21388e36a86932fdce5d39
yaoyk <yyk1052433260@163.com>
2019-04-29 19:55:15 +0800

    [IMP] 解决如何过滤返回集的问题

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -18,16 +18,26 @@ COLOR_CLASS_DICT = {
 }
 
 TAG_TH_STR = """
-<th {color} >{char}</th>
+<th {color} {index} >{char}</th>
 """
+
+STR_TH_ID = """
+id="th-0-{}"
+"""
+
 TAG_TD_STR = """
-<td {color} >{char}</td>
+<td {color} {index} >{char}</td>
+"""
+
+STR_TD_ID = """
+id="td-{col}-{row}"
 """
 
 
 @register.simple_tag
 def get_field_desc(model_class, field_name, front=True):
     try:
+        # noinspection PyProtectedMember
         result = model_class._meta.get_field(field_name).verbose_name
     except Exception as e:
         __logger__.error(e)
@@ -36,18 +46,31 @@ def get_field_desc(model_class, field_name, front=True):
 
 
 @register.simple_tag
-def generate_table_header(model_class, field_name, front=True):
+def generate_table_header(model_class, field_name, index='', front=True):
     _desc = get_field_desc(model_class, field_name, True)
     _color = 'none'
 
-    _tag = TAG_TH_STR.format(color=COLOR_CLASS_DICT.get(_color, ''), char=_desc)
+    # 开启id
+    _id_tag = False
+    if not _id_tag:
+        index = ''
+    else:
+        index = STR_TH_ID.format(index)
+
+    _tag = TAG_TH_STR.format(color=COLOR_CLASS_DICT.get(_color, ''), index=index, char=_desc)
     return mark_safe(_tag) if front else _tag
 
 
 @register.simple_tag
-def generate_table_body(model_obj, field_name, front=True):
+def generate_table_body(model_obj, field_name, col_index='', row_index='', front=True):
     _data = getattr(model_obj, field_name, '')
     _color = 'none'
+
+    _id_tag = False
+    if not _id_tag:
+        index = ''
+    else:
+        index = STR_TD_ID.format(col=col_index, row=row_index)
 
     if field_name == 'score_level':
         if _data == 'A':
@@ -55,5 +78,10 @@ def generate_table_body(model_obj, field_name, front=True):
         if _data == 'D':
             _color = 'danger'
 
-    _tag = TAG_TD_STR.format(color=COLOR_CLASS_DICT.get(_color, ''), char=_data)
+    _tag = TAG_TD_STR.format(color=COLOR_CLASS_DICT.get(_color, ''), index=index, char=_data)
     return mark_safe(_tag) if front else _tag
+
+
+@register.simple_tag
+def get_col_index(index, front=True):
+    return mark_safe(index) if front else index
