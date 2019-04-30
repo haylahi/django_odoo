4c5c0b1bff5319be4f2839070a55c0c3c30e8c82
yaoyk <yyk1052433260@163.com>
2019-04-30 11:53:26 +0800

    [IMP] get room name

--- chatting/views.py
+++ chatting/views.py
@@ -1,7 +1,16 @@
+import json
+
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 
 def index(request):
     return render(request, 'chat/index.html', {
         'frontUrl': '/chat/'
+    })
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'roomName': mark_safe(room_name),
     })
