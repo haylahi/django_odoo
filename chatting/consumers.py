2b88449f1a9efd448621f4d5177355a89e21a8a2
yaoyk <yyk1052433260@163.com>
2019-04-30 17:49:30 +0800

    [IMP] commit consumer

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -3,30 +3,42 @@
 import json
 from datetime import datetime
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 from base.utils import FORMAT_DATETIME
 
 
 class ChatConsumer(WebsocketConsumer):
-    """
-    web socket 协议
-
-    """
-
     def connect(self):
-        print('recv a conn')
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_{n}'.format(n=self.room_name)
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, code):
-        print('a coon leave : ', code)
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        print('recv data is ', text_data_json)
-        message = text_data_json['message']
-        now_date = datetime.now().strftime(FORMAT_DATETIME)
+        _message = text_data_json.get('message')
+        _now = datetime.now().strftime(FORMAT_DATETIME)
 
-        self.send(text_data=json.dumps({
-            'message': '{} --> {}'.format(message, now_date)
-        }))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {
+                'type': 'chat_message',
+                'message': '{}{}'.format(_message, _now)
+            }
+        )
+
+    def chat_message(self, event):
+        _msg = event.get('message')
+
+        # 发送到WebSocket
+        self.send(text_data=json.dumps({'message': _msg}))
